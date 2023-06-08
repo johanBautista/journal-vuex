7525f2a98b9748b03b020ceb4cee6106c381801d
Johan Bautista <jsbparra@gmail.com>
2023-06-08 16:22:38 +0200

    instal sass and modify css bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <button class="btn btn-primary">jee</button>
+  <router-view />
 </template>
 
 <style>
